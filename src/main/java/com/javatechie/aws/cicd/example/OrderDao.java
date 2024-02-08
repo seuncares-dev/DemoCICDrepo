f88c7e0b6f4fcdaebe1c2dd0d8aef02a92f0278f
seuncares <mattseun02@gmail.com>
2024-02-07 22:26:13 -0500

    initial commit

---
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -0,0 +1,25 @@
+package com.javatechie.aws.cicd.example;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository
+public class OrderDao {
+
+
+    public List<Order> getOrders() {
+        return Stream.of(
+                new Order(101, "Mobile", 1, 30000),
+                new Order(58, "Book", 4, 2000),
+                new Order(205, "Laptop", 1, 150000),
+                new Order(809, "headset", 1, 1799),
+                new Order(111, "Car", 1, 200000),
+                new Order(112, "TV", 5, 400000),
+                new Order(113, "Fridge", 6, 500000),
+                new Order(114, "Home Theater", 7, 1000))
+                .collect(Collectors.toList());
+    }
+}
